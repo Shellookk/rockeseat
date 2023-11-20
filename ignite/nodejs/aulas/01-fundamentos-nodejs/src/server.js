a1617500ea32d53884203b7681fc2cc8a9e08bad
Isaque Rodrigues Rocha <isauqejk123@gmail.com>
2023-11-20 15:30:06 -0300

    commit para subir ajustes

--- ignite/nodejs/aulas/01-fundamentos-nodejs/src/server.js
+++ ignite/nodejs/aulas/01-fundamentos-nodejs/src/server.js
@@ -1,18 +1,31 @@
 import http from 'node:http'
 //commonJS => require
 //ESmodules => import/export
+const users = []
+
 const server = http.createServer((req, res) =>{
     const{ method, url} = req
 
     if (method ==='GET' && url==='/users') {
-        return res.end('Listagem de usuários')
+        return res
+        .setHeader('Content-type', 'application/json')
+        .end(JSON.stringify(users))
+
+
     }
 
     if (method=== 'POST' && url ==='/users') {
-        return res.end('Criação de usuário')
-    }
-    
-    return res.end('Hello World')
+        users.push({
+            id: 1,
+            name:'John Doe',
+            email: 'Johndoe@example.com '
+        })
+
+        return res.writeHead(201).end()
+
+}
+
+return res.writeHead(404).end()
 })
 
 server.listen(3333)
@@ -26,3 +39,10 @@ server.listen(3333)
 // POST => CRIAR UMA INFORMAÇÃO NO BACK-END
 // PUT => CRIAR UM RECURSO NO BACK-END
 // PATCH => ATUALIZA UMA INFORMAÇÃO ESPECÍFICA DE UM RECURSO NO BACK-END
+
+// Stateful - Stateless
+// Stateful - Salva os dados na memória, stateless não salva nada em memória, salva em dispositivcos externos.
+
+// Cabeçalhos (Requisição/Resposta) => metadados
+
+// http status code
