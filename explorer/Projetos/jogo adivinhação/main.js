a518ca5a1a7338be0a34ea002b923a229ee81191
Shellookk <isauqejk123@gmail.com>
2024-03-27 11:42:26 -0300

    atualizações

---
+++ explorer/Projetos/jogo adivinhação/main.js
@@ -0,0 +1,27 @@
+const randomNumber = Math.round(Math.random()*10);
+
+let xAttempts = 1;
+
+
+
+function handleClick(event){
+    
+    event.preventDefault()
+
+    const inputNumber = document.querySelector("#inputNumber")
+
+    if (Number(inputNumber.value) == randomNumber) {
+        document.querySelector(".screen1").classList.add("hide");
+        document.querySelector(".screen2").classList.remove("hide");
+
+        document.querySelector(".screen2 h2").innerText = `Acertou em ${xAttempts} Tentativas!`;
+
+        console.log(`Você acertou em: ${xAttempts} "Tentativas`);
+    }
+    
+    xAttempts++
+
+    console.log(xAttempts)
+}
+
+
