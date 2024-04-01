4f3b4b399d821287960ce9f5dd311e6ccb6cf5f4
Shellookk <isauqejk123@gmail.com>
2024-04-01 17:11:19 -0300

    Atualizações

--- explorer/Projetos/jogo adivinhação/main.js
+++ explorer/Projetos/jogo adivinhação/main.js
@@ -1,27 +1,44 @@
+// Variaveis
+const screen1 = document.querySelector(".screen1");
+const screen2 = document.querySelector(".screen2");
+const btnTry = document.querySelector("#btnTry");
+const btnReset = document.querySelector("#btnReset");
+
 const randomNumber = Math.round(Math.random()*10);
+
 
 let xAttempts = 1;
 
 
-
-function handleClick(event){
-    
+//função callback
+function handleTryClick(event){
     event.preventDefault()
 
     const inputNumber = document.querySelector("#inputNumber")
 
     if (Number(inputNumber.value) == randomNumber) {
-        document.querySelector(".screen1").classList.add("hide");
-        document.querySelector(".screen2").classList.remove("hide");
+      screen1.classList.add("hide");
+      screen2.classList.remove("hide");
 
         document.querySelector(".screen2 h2").innerText = `Acertou em ${xAttempts} Tentativas!`;
 
         console.log(`Você acertou em: ${xAttempts} "Tentativas`);
     }
     
-    xAttempts++
+    xAttempts++;
 
-    console.log(xAttempts)
-}
+    inputNumber.value = "";
 
+    console.log(xAttempts);
+};
+function handleResetClick(){
+    screen1.classList.remove("hide");
+    screen2.classList.add("hide");
+    xAttempts = 1;
+};
 
+// eventos
+
+btnTry.addEventListener("click", handleTryClick);
+
+btnReset.addEventListener("click", handleResetClick );
