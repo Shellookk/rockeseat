3e417d7095afdbbcc668a5cacf6b6a67a3e992dd
Shellookk <isauqejk123@gmail.com>
2024-04-13 18:51:11 -0300

    subindo atualizações

--- explorer/Projetos Stage 5/Projeto IMC/main.js
+++ explorer/Projetos Stage 5/Projeto IMC/main.js
@@ -1,14 +1,38 @@
-// variaveis
+// variáveis - variables
+const form = document.querySelector('form')
+const inputWeight = document.querySelector('#weight')
+const inputHeight = document.querySelector('#height')
 
-const form = document.querySelector('form');
-const inputPeso = document.querySelector('#peso')
-const inputAltura = document.querySelector('#altura')
+const Modal = {
+    wrapper: document.querySelector('.modal-wrapper'),
+    message: document.querySelector('.modal .title span'),
+    buttonClose: document.querySelector('.modal button.close'),
 
+    open() {
+        Modal.wrapper.classList.add('open')
+    },
+    close() {
+        Modal.wrapper.classList.remove('open')
+    }
+}
 
-form.onsubmit = function (event){
+form.onsubmit = event => {
     event.preventDefault()
 
-    const peso = inputPeso.value
-    const altura = inputAltura.value
+    const weight = inputWeight.value
+    const height = inputHeight.value
 
+    const result = IMC(weight, height)
+    const message = `Seu IMC é de ${result}`
+
+    Modal.message.innerText = message
+    Modal.open()
+}
+
+Modal.buttonClose.onclick = () => {
+    Modal.close()
+}
+
+function IMC(weight, height) {
+    return (weight / ((height / 100) ** 2)).toFixed(2)
 }
