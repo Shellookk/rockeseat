35f7e1175710b2137267685ea69c2de1b4638c48
Shellookk <isauqejk123@gmail.com>
2024-04-17 21:02:32 -0300

    subindo ajustes

--- explorer/desafios/SPA Universe/Js/main.js
+++ explorer/desafios/SPA Universe/Js/main.js
@@ -7,20 +7,8 @@ const buttonUniverse = document.querySelector('universe')
 const buttonExplorer = document .querySelector('explorer')
 const buttonMoreInfo = document.querySelector('#moreInfo')
 
-
-
 // Botão saiba mais
 buttonMoreInfo.addEventListener("click", function(){
     window.location.href ="/universe"
 })
 
-const router = new Router()
-router.add("/", "/pages/home.html")
-router.add("/home", "/pages/home.html")
-router.add("/universe", "/pages/universe.html")
-router.add("/explorer", "/pages/explorer.html")
-
-router.handle()
-
-window.onpopstate = () => router.handle()
-window.Route = () => router.Route()
