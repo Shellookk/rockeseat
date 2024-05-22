d04318e18cb4216ccc4c5991472d04dd9d806db2
Shellookk <isauqejk123@gmail.com>
2024-05-22 12:16:13 -0300

    atualizações

---
+++ explorer/Projetos/Projeto 11/rocketnotes/src/hooks/auth.jsx
@@ -0,0 +1,20 @@
+import { createContext } from 'react';
+
+export const AuthContext = createContext({})
+
+function AuthProvider({ children }){
+
+    return(
+        <AuthContext.Provider value={{ }}>    
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+function useAuth(){
+    const context = useContext(AuthContext)
+
+    return context
+}
+
+export {AuthProvider, useAuth}
